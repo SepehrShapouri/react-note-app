2902564f36c5637d3766dd361611ceb86fb7eb1d
SepehrShapouri <110922785+SepehrShapouri@users.noreply.github.com>
2023-08-24 12:39:13 +0330

    responsive

--- src/components/NoteFilters.jsx
+++ src/components/NoteFilters.jsx
@@ -1,13 +1,20 @@
-const NoteFilters = ({allNotes}) => {
-  const completed = allNotes.filter(item => item.completed == true)
-  const unCompleted = allNotes.filter(item => item.completed != true)
-    return ( 
-        <ul className="notes-filter">
-        <li className="note-filter-option">All <span className="filter-status">{allNotes.length}</span></li>
-        <li className="note-filter-option">Completed <span className="filter-status">{completed.length}</span></li>
-        <li className="note-filter-option">Open <span className="filter-status">{unCompleted.length}</span></li>
-      </ul>
-     );
-}
- 
+const NoteFilters = ({ allNotes }) => {
+  const completed = allNotes.filter((item) => item.completed == true);
+  const unCompleted = allNotes.filter((item) => item.completed != true);
+  if (!allNotes.length) return <h2>No Notes Added</h2>;
+  return (
+    <ul className="notes-filter">
+      <li className="note-filter-option">
+        All <span className="filter-status">{allNotes.length}</span>
+      </li>
+      <li className="note-filter-option">
+        Completed <span className="filter-status">{completed.length}</span>
+      </li>
+      <li className="note-filter-option">
+        Open <span className="filter-status">{unCompleted.length}</span>
+      </li>
+    </ul>
+  );
+};
+
 export default NoteFilters;
